b2754432ed16a71638e35b8588b072e6217e29ba
surajmohe <surajmohe01@gmail.com>
2024-03-04 23:18:10 +0530

    For this number 188.68515971267004, show only 2 digits after the decimal using toFixed() method.

--- Question1.js
+++ Question1.js
@@ -3,4 +3,9 @@
 /* Question 1: For this number 188.68515971267004, show only 2 digits after the decimal using toFixed() method.
 */
 
+let number=188.68515971267004;
+
+let b=number.toFixed(2);
+
+console.log(b);
 
