02277e791c0ff2b3265aafabf7138aae2eb43efa
surajmohe <surajmohe01@gmail.com>
2024-03-05 14:10:29 +0530

    For this number 5.123456, show only 3 significant digits using toPrecision() method

--- Question2.js
+++ Question2.js
@@ -2,5 +2,8 @@
       
 /* Question 2:  For this number 5.123456, show only 3 significant digits using toPrecision() method.
 */
+let number = 5.123456;
+let b = number.toPrecision(3)
 
+console.log(b);
 
