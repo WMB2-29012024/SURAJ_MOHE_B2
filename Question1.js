551c658f9d7a77011761eff2bfe1c4a57f8502bc
surajmohe <surajmohe01@gmail.com>
2024-03-07 16:10:24 +0530

    Question Compelte

--- Question1.js
+++ Question1.js
@@ -15,8 +15,10 @@
 // Expected Output: 25
 // Justification: Summing up the two diagonals (1+5+9+3+7), we get 25. Please note that the element at [1][1] = 5 is counted only once.
 
-const arr = [[1,2,3],[4,5,6],[7,8,9]]
 
+const arr =[[1,2,3],
+[4,5,6],
+[7,8,9]]
 const diagonalSum = (arr) =>{
      let sum =0;
      for (let i = 0; i < arr.length; i++) {
@@ -24,8 +26,13 @@ const diagonalSum = (arr) =>{
            sum +=arr[i][i] +arr[i][arr[i].length -i-1]
          
      }
+
+if (arr.length %2 !== 0) {
      let middel   = Math.floor(arr.length/2)
      sum -= arr[middel][middel]
+}
+
+     
     return sum;
 }
 console.log(diagonalSum(arr));
