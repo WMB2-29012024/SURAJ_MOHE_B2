13808770d9cad0a734e25799b1373e0f81cc9b04
surajmohe <surajmohe01@gmail.com>
2024-03-07 14:38:52 +0530

    Question complete

--- Question1.js
+++ Question1.js
@@ -1,24 +1,26 @@
-// Given a one-dimensional array of integers, create a new array that represents the running sum of the original array.
-// The running sum at position i in the new array is calculated as the sum of all the numbers in the original array from the 0th index up to the i-th index (inclusive). Formally, the resulting array should be computed as follows: result[i] = sum(nums[0] + nums[1] + ... + nums[i]) for each i from 0 to the length of the array minus one.
-
-// Examples
-
-// Problem Statement
-// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+// Given an input array of integers nums, find an integer array, let’s call it differenceArray, of the same length as an input integer array.
+// Each element of differenceArray, i.e., differenceArray[i], should be calculated as follows: take the sum of all elements to the left of index i in array nums (denoted as leftSum[i]), and subtract it from the sum of all elements to the right of index i in array nums (denoted as rightSum[i]), taking the absolute value of the result. Formally:
+// differenceArray[i] = | leftSum[i] - rightSum[i] |
+// If there are no elements to the left/right of i, the corresponding sum should be taken as 0.
 
 
+const arr = [2, 5, 1, 6];
 
-const num = [1, 2, 3, 4];
-
-const hasDuplicate = (nums) => {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] === nums[j]) {
-                return true;
-            }
+let sumCountElemt = (arr) =>{
+    let leftOfSum = 0;
+    let rightOfSum = 0;
+    let arr2 = [];
+    for (let i = 0; i <arr.length; i++) {
+        // console.log(arr[i]);
+        let currentElem = arr[i];
+        for (let j = i; j < arr.length; j++) {
+                console.log(arr[j]);
         }
+
+
     }
-    return false;
+
 }
 
-console.log(hasDuplicate(num));
+console.log(sumCountElemt(arr));
+
