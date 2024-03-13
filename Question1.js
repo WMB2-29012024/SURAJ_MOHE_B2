d8aecdf71a9ba981ac6a3049cc61fc10bc24b7b2
surajmohe <surajmohe01@gmail.com>
2024-03-13 15:59:31 +0530

    complete simplify path

--- Question1.js
+++ Question1.js
@@ -1,13 +1,13 @@
-// Problem Statement
+// Given an absolute file path in a Unix-style file system, simplify it by converting “..” to the previous directory and removing any “.” or multiple slashes. The resulting string should represent the shortest absolute path.
 
-// Given a string, write a function that uses a stack to reverse the string. The function should return the reversed string.
+// Examples:
 
-// Examples
+// Example 1.
 
-// Example 1:
+// Input: "/a//b////c/d//././/.."
+// Output: "/a/b/c"
 
-// Input: "Hello, World!"
-// Output: "!dlroW ,olleH"
+
 class Stack{
      constructor(size){
        this.stack =[] ;
@@ -52,20 +52,28 @@ class Stack{
           return this.removeItem
      }
 }
+const s = "/a//b////c/d//././/..";
+const stackOfArray = (s) =>{
+    const stack = new Stack(s.length);
+     let a = s.split("/")
 
-const s = "Hello, World!";
-
-const stringWorld =(str) =>{
-     let string = "";
-     let stack = new Stack(s.length)
-     for (let i = 0; i<str.length; i++) {
-          stack.stackPush(str[i])
+     for (let i = 0; i <a.length; i++) {
+          if(a[i]>="a" && a[i]<="z"){
+               stack.stackPush(a[i])
+          }
+          else if(a[i] ==='..'){
+               stack.stackPop(a[i])
+          }
      }
-     let abc ="";
+
+     let  ans = "";
+
      while(!stack.isEmpty()){
-          abc += stack.stackPop();
+          ans = "/"+stack.stackPop()+ans;
      }
-     return abc;
-}
 
-console.log(stringWorld(s));
+
+     return ans;
+
+}
+console.log(stackOfArray(s));
