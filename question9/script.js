948d93c3fb9d22781148de3619380709af1af8be
surajmohe <surajmohe01@gmail.com>
2024-04-08 17:21:49 +0530

    complete all questions

--- question9/script.js
+++ question9/script.js
@@ -1,15 +1,27 @@
 const ul = document.querySelector('ul');
 const btn = document.getElementById('next-btn');
 const pre = document.getElementById('pre-btn');
+const loader = document.getElementById("loader")
 let dataId = 1;
 
 async function displayData() {
+  
     try {
         const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${dataId}`);
         const data = await response.json();
         console.log(data);
+        const checkbox = document.createElement("input")
+        checkbox.type = "checkbox"
+        if(data.completed == true){
+            checkbox.checked =true
+        }
         const listItem = document.createElement('li');
+        const  listItem2 = document.createElement('p');
+        listItem2.innerHTML = `userId : ${data.id}`;
+        console.log(listItem2);
         listItem.innerHTML = `Title : ${data.title}`;
+        ul.appendChild(checkbox)
+        ul.appendChild(listItem2);
         ul.appendChild(listItem);
     } catch (error) {
         console.log('no data to fetch');
@@ -19,13 +31,19 @@ async function displayData() {
 btn.addEventListener('click', async () => {
     dataId += 1;
     ul.innerHTML = '';
+    loader.style.display = "inline-block"
     await displayData();
+    loader.style.display = "none"
+
 });
 
 pre.addEventListener('click', async () => {
     dataId -= 1;
     ul.innerHTML = '';
+    loader.style.display = "inline-block"
     await displayData();
+    loader.style.display = "none"
+
 })
 
 displayData();
